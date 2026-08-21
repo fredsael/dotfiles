147356ded2726d53f2f98bf3f4b15b4d311121f7
Fredrik <fredrik.m.saelen@proton.me>
2026-08-21 14:24:46 +0200

    Added hypr to dotfiles

---
+++ hypr/.config/hypr/config/inputs.lua
@@ -0,0 +1,23 @@
+-- Input configuration
+
+hl.config({
+    input = {
+        -- sensitivity = -0.25,
+        accel_profile = "flat",
+        kb_layout = "no",
+
+	touchpad = { natural_scroll = true}
+    },
+
+   --touchpad {natural_scroll = 1}, 
+    -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
+    -- cursor = {
+    --     no_hardware_cursors = 1,
+    -- },
+})
+
+hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "down",       action = "close" })
+hl.gesture({ fingers = 3, direction = "up",         action = "fullscreen" })
+hl.gesture({ fingers = 3, direction = "left",       action = "float" })
+
